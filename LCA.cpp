901bc5988adf2b065f4ac53e9ffc33318a8693c2
Sadat-Hossain-01 <sadatho2014@gmail.com>
2021-04-24 13:06:12 +0600

    Update LCA.cpp

--- LCA.cpp
+++ LCA.cpp
@@ -86,6 +86,31 @@ void dfs(int v, int parent) {
         dfs(neighbour, v);
     }
 }
+int get_distance_between_nodes_of_a_tree(int a, int b) {
+    static bool isDFSdone = false;
+    if (!isDFSdone) {
+        dfs(1, -1);
+        isDFSdone = true;
+    }
+    if (depth[a] < depth[b]) swap(a, b);
+    int distance = 0;
+    int k = depth[a] - depth[b];
+    for (int j = LOG - 1; j >= 0; j--) {
+        if (k & (1 << j)) {
+            distance += (1 << j);
+            a = up[a][j];
+        }
+    }
+    if (a == b) return distance;
+    for (int j = LOG - 1; j >= 0; j--) {
+        if (up[a][j] != up[b][j]) {
+            a = up[a][j];
+            b = up[b][j];
+            distance += 2 * (1 << j);
+        }
+    }
+    return distance + 2;
+}
 int get_LCA(int a, int b) {
     static bool isDFSdone = false;
     if (!isDFSdone) {
