102301614288d5a6eac2717a6d1ef9300c3b60da
Sadat-Hossain-01 <sadatho2014@gmail.com>
2021-04-24 12:54:57 +0600

    Update LCA.cpp

--- LCA.cpp
+++ LCA.cpp
@@ -59,9 +59,11 @@ const int MX = 10000;
 const int LOG = __lg(MX - 1) + 1;
 int n;
 vector<vector<int>>children(MX);
+vector<vector<int>>adjacent(MX);
 vector<vector<int>>up(MX, vector<int>(LOG)); //up[i][j] is the 2^j th ancestor of i
 vector<int>depth(MX);
 
+//this version is applicable if inputs are given in node-children form
 void dfs(int v) {
     for (auto child : children[v]) {
         depth[child] = depth[v] + 1;
@@ -72,10 +74,23 @@ void dfs(int v) {
         dfs(child);
     }
 }
+//this version is applicable if inputs are given in edge form
+void dfs(int v, int parent) {
+    for (auto neighbour : adjacent[v]) {
+        if (neighbour == parent) continue;
+        depth[neighbour] = depth[v] + 1;
+        up[neighbour][0] = v;
+        for (int j = 1; j < LOG; j++) {
+            up[neighbour][j] = up[up[neighbour][j - 1]][j - 1];
+        }
+        dfs(neighbour, v);
+    }
+}
 int get_LCA(int a, int b) {
     static bool isDFSdone = false;
     if (!isDFSdone) {
         dfs(0);
+        //or, dfs(0, -1);
         isDFSdone = true;
     }
     if (depth[a] < depth[b]) swap(a, b);
