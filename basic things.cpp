ba3f7198701095ac348a67988df6f634b7a56daa
Sadat-Hossain-01 <sadatho2014@gmail.com>
2021-05-11 15:50:28 +0600

    Update basic things.cpp

--- basic things.cpp
+++ basic things.cpp
@@ -88,8 +88,21 @@ int power(int a, int b) {
     }
     return ans;
 }
+int gcd(int a, int b, int& x, int& y) {
+    x = 1, y = 0;
+    int x1 = 0, y1 = 1, a1 = a, b1 = b;
+    while (b1) {
+        int q = a1 / b1;
+        tie(x, x1) = make_tuple(x1, x - q * x1);
+        tie(y, y1) = make_tuple(y1, y - q * y1);
+        tie(a1, b1) = make_tuple(b1, a1 - q * b1);
+    }
+    return a1;
+}
 inline int inverse(int a) {
-    return power(a, MOD - 2);
+    int x, y;
+    int whatever = gcd(a, MOD, x, y);
+    return x;
 }
 inline int divide(int a, int b) {
     return mul(a, inverse(b));
