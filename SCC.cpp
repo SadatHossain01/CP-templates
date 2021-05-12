9076a565fbb692827f998a3a0691a997bdafb269
Sadat-Hossain-01 <sadatho2014@gmail.com>
2021-05-13 00:50:03 +0600

    Create SCC.cpp

---
+++ SCC.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+int n, m, a, b, timer = 1, SCC;
+vector<vector<int>>adj;
+vector<bool>visited, onStack;
+vector<int>inTime, lowLink;
+stack<int>st;
+
+void tarjan(int node) {
+    visited[node] = true;
+    inTime[node] = lowLink[node] = timer++;
+    onStack[node] = true;
+    st.push(node);
+    for (int neighbour : adj[node]) {
+        if (visited[neighbour]) {
+            if (onStack[neighbour]) {
+                lowLink[node] = min(lowLink[node], inTime[neighbour]);
+            }
+        }
+        else {
+            tarjan(neighbour);
+            if (onStack[neighbour]) {
+                lowLink[node] = min(lowLink[node], lowLink[neighbour]);
+            }
+        }
+    }
+    if (inTime[node] == lowLink[node]) {
+        cout << "SCC #" << ++SCC << endl;
+        int u;
+        while (true) {
+            u = st.top();
+            st.pop();
+            onStack[u] = false;
+            cout << u << " ";
+            if (u == node) break;
+        }
+        cout << endl;
+    }
+}
+
+void solve()
+{
+    cin >> n >> m;
+    adj.resize(n + 1);
+    visited.assign(n + 1, false);
+    onStack.assign(n + 1, false);
+    inTime.resize(n + 1);
+    lowLink.resize(n + 1);
+    for (int i = 1; i <= m; i++) {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+    }
+
+    for (int i = 1; i <= n; i++)
+        if (visited[i] == false) tarjan(i);
+}
