96589420e0cb90c1d43a9b6643abc23443fbf6a7
Sadat-Hossain-01 <sadatho2014@gmail.com>
2022-07-06 18:15:53 +0600

    .

--- My ones/Mo's Algorithm for # of unique elements.cpp
+++ My ones/Mo's Algorithm for # of unique elements.cpp
@@ -3,76 +3,66 @@ using namespace std;
 
 #define endl "\n"
 typedef long long ll;
-
-// SPOJ Problem DQUERY
-
+// Solves SPOJ Problem DQUERY
 struct Query {
     int l, r, index, ans;
 };
 const int BLOCK_SIZE = 200;
-const int MAX_VALUE = 1e6 + 10;
-int n, q, ans;
+const int MX = 1e6 + 10;
+int n, q, n_unique;
 int current_left = 0, current_right = -1;
-vector<int> numbers;
-vector<int> frequency(MAX_VALUE);
-vector<Query> questions;
+vector<int> numbers, frequency(MX);
+vector<Query> queries;
 void add(int index) {
-    if (index < 0 || index >= (int)numbers.size()) return;
+    if (index < 0 || index >= numbers.size()) return;
     int element = numbers[index];
     frequency[element]++;
-    if (frequency[element] == 1) {
-        ans++;
-    }
+    if (frequency[element] == 1) n_unique++;
 }
 void remove(int index) {
-    if (index < 0 || index >= (int)numbers.size()) return;
+    if (index < 0 || index >= numbers.size()) return;
     int element = numbers[index];
     frequency[element]--;
-    if (frequency[element] == 0) ans--;
+    if (frequency[element] == 0) n_unique--;
 }
-void Mo(Query& that) {
-    while (current_left > that.l) {
+void processQueries(Query& q) {
+    while (current_left > q.l) {
         current_left--;
         add(current_left);
     }
-    while (current_right < that.r) {
+    while (current_right < q.r) {
         current_right++;
         add(current_right);
     }
-    while (current_left < that.l) {
+    while (current_left < q.l) {
         remove(current_left);
         current_left++;
     }
-    while (current_right > that.r) {
+    while (current_right > q.r) {
         remove(current_right);
         current_right--;
     }
-    that.ans = ans;
+    q.ans = n_unique;
 }
 void solve() {
-    scanf("%d", &n);
+    cin >> n;
     numbers.resize(n);
-    for (int i = 0; i < n; i++) scanf("%d", &numbers[i]);
-    scanf("%d", &q);
-    questions.resize(q);
+    for (int i = 0; i < n; i++) cin >> numbers[i];
+    cin >> q;
+    queries.resize(q);
     for (int i = 0; i < q; i++) {
-        scanf("%d%d", &questions[i].l, &questions[i].r);
-        questions[i].l--;
-        questions[i].r--;
-        questions[i].index = i;
+        cin >> queries[i].l >> queries[i].r;
+        queries[i].l--;
+        queries[i].r--;
+        queries[i].index = i;
     }
-    sort(questions.begin(), questions.end(),
-         [&](const Query& p, const Query& q) {
-             if (p.l / BLOCK_SIZE != q.l / BLOCK_SIZE)
-                 return make_pair(p.l, p.r) < make_pair(q.l, q.r);
-             return (p.l / BLOCK_SIZE & 1) ? (p.r < q.r) : (p.r > q.r);
-         });
-    for (int i = 0; i < q; i++) {
-        Mo(questions[i]);
-    }
-    sort(questions.begin(), questions.end(),
+    sort(queries.begin(), queries.end(), [&](const Query& p, const Query& q) {
+        if (p.l / BLOCK_SIZE != q.l / BLOCK_SIZE)
+            return make_pair(p.l, p.r) < make_pair(q.l, q.r);
+        return (p.l / BLOCK_SIZE & 1) ? (p.r < q.r) : (p.r > q.r);
+    });
+    for (int i = 0; i < q; i++) processQueries(queries[i]);
+    sort(queries.begin(), queries.end(),
          [&](const Query& p, const Query& q) { return p.index < q.index; });
-    for (int i = 0; i < q; i++) {
-        printf("%d\n", questions[i].ans);
-    }
+    for (int i = 0; i < q; i++) cout << queries[i].ans << endl;
 }
--- My ones/Mo's Algorithm for Highest Frequency.cpp
+++ My ones/Mo's Algorithm for Highest Frequency.cpp
@@ -1,83 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define endl "\n"
 typedef long long ll;
-
 struct Query {
     int l, r, index, ans;
 };
 const int BLOCK_SIZE = 350;
-const int MAX_VALUE = 1e5 + 10;
-int n, q, ans = 1;
+const int MX = 1e5 + 10;
+int n, q, max_freq = 1;
 int current_left = 0, current_right = -1;
-vector<int> numbers;
-vector<int> frequency(MAX_VALUE);
-vector<int> freq_frequency;
-vector<Query> questions;
+vector<int> numbers, frequency(MX), freq_frequency;
+vector<Query> queries;
 void add(int index) {
-    if (index < 0 || index >= (int)numbers.size()) return;
+    if (index < 0 || index >= numbers.size()) return;
     int element = numbers[index];
     int previous_frequency = frequency[element];
     frequency[element]++;
     int current_frequency = frequency[element];
-    if (previous_frequency) freq_frequency[previous_frequency]--;
+    if (previous_frequency > 0) freq_frequency[previous_frequency]--;
     freq_frequency[current_frequency]++;
-    if (freq_frequency[current_frequency] == 1 && current_frequency > ans)
-        ans = current_frequency;
+    if (freq_frequency[current_frequency] == 1 && current_frequency > max_freq)
+        max_freq = current_frequency;
 }
 void remove(int index) {
-    if (index < 0 || index >= (int)numbers.size()) return;
+    if (index < 0 || index >= numbers.size()) return;
     int element = numbers[index];
     int previous_frequency = frequency[element];
     frequency[element]--;
     int current_frequency = frequency[element];
-    if (current_frequency) freq_frequency[current_frequency]++;
+    if (current_frequency > 0) freq_frequency[current_frequency]++;
     freq_frequency[previous_frequency]--;
-    if (ans == previous_frequency && freq_frequency[previous_frequency] == 0)
-        ans = current_frequency;
+    if (max_freq == previous_frequency &&
+        freq_frequency[previous_frequency] == 0)
+        max_freq = current_frequency;
 }
-void Mo(Query& that) {
-    while (current_left > that.l) {
+void processQueries(Query& q) {
+    while (current_left > q.l) {
         current_left--;
         add(current_left);
     }
-    while (current_right < that.r) {
+    while (current_right < q.r) {
         current_right++;
         add(current_right);
     }
-    while (current_left < that.l) {
+    while (current_left < q.l) {
         remove(current_left);
         current_left++;
     }
-    while (current_right > that.r) {
+    while (current_right > q.r) {
         remove(current_right);
         current_right--;
     }
-    that.ans = ans;
+    q.ans = max_freq;
 }
 void solve() {
-    scanf("%d%d", &n, &q);
+    cin >> n >> q;
     numbers.resize(n);
-    freq_frequency.assign(n + 5, 0);
-    for (int i = 0; i < n; i++) scanf("%d", &numbers[i]);
-    questions.resize(q);
+    freq_frequency.assign(n + 2, 0);
+    for (int i = 0; i < n; i++) cin >> numbers[i];
+    queries.resize(q);
     for (int i = 0; i < q; i++) {
-        scanf("%d%d", &questions[i].l, &questions[i].r);
-        questions[i].index = i;
+        cin >> queries[i].l >> queries[i].r;
+        queries[i].index = i;
     }
-    sort(questions.begin(), questions.end(),
-         [&](const Query& p, const Query& q) {
-             if (p.l / BLOCK_SIZE != q.l / BLOCK_SIZE)
-                 return make_pair(p.l, p.r) < make_pair(q.l, q.r);
-             return (p.l / BLOCK_SIZE & 1) ? (p.r < q.r) : (p.r > q.r);
-         });
-    for (int i = 0; i < q; i++) {
-        Mo(questions[i]);
-    }
-    sort(questions.begin(), questions.end(),
+    sort(queries.begin(), queries.end(), [&](const Query& p, const Query& q) {
+        if (p.l / BLOCK_SIZE != q.l / BLOCK_SIZE)
+            return make_pair(p.l, p.r) < make_pair(q.l, q.r);
+        return (p.l / BLOCK_SIZE & 1) ? (p.r < q.r) : (p.r > q.r);
+    });
+    for (int i = 0; i < q; i++) processQueries(queries[i]);
+    sort(queries.begin(), queries.end(),
          [&](const Query& p, const Query& q) { return p.index < q.index; });
     for (int i = 0; i < q; i++) {
-        printf("%d\n", questions[i].ans);
+        cout << queries[i].ans << endl;
     }
 }
